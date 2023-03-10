c5f363ce72bb3ec3fac410cc84f7e2fc024f27e4
Choryu Park (Kyoungkyu Park) <choryu.park@choryu.space>
2023-03-11 04:22:14 +0900

    Add support 'AM8 Switchwire with SKR mini e3 v3.0'

--- Marlin/src/pins/stm32g0/pins_BTT_SKR_MINI_E3_V3_0.h
+++ Marlin/src/pins/stm32g0/pins_BTT_SKR_MINI_E3_V3_0.h
@@ -343,7 +343,7 @@
       #define BTN_ENC                EXP1_02_PIN
       #define BTN_EN1                EXP1_06_PIN
       #define BTN_EN2                EXP1_01_PIN
-      #define BEEPER_PIN                    -1
+      #define BEEPER_PIN                     PD0
 
       #define DOGLCD_CS              EXP1_03_PIN
       #define DOGLCD_A0              EXP1_05_PIN
