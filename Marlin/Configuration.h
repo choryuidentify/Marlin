3c975e54abaaf5c7f05de9cae6f7418b84512ecd
Choryu Park <choryu.park@choryu.space>
2025-12-14 12:42:14 +0900

    Update pid calibration value

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -791,9 +791,9 @@
   //#define PID_BED_DEBUG // Print Bed PID debug data to the serial port.
 
   // Anet A8 Original Heat Bed + 2mm Epoxy Bed + Ender Lightweight Chamber
-  #define DEFAULT_bedKp 70.83
-  #define DEFAULT_bedKi 5.69
-  #define DEFAULT_bedKd 587.77
+  #define DEFAULT_bedKp 205.65
+  #define DEFAULT_bedKi 37.60
+  #define DEFAULT_bedKd 749.93
 
   // 120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   // from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
